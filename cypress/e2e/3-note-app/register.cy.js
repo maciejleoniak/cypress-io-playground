da03071397d5bae31569e6bc9c687abfc6b40bbb
7799043 <leoniak.maciek1@gmail.com>
2023-06-22 15:22:09 +0200

    .

--- cypress/e2e/3-note-app/register.cy.js
+++ cypress/e2e/3-note-app/register.cy.js
@@ -1,21 +1,31 @@
 context('register', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
-  })
+    cy.visit('http://localhost:3000');
+  });
 
-  it('pop up click', () => {
-    cy.get('#theme-toggle-button').click()
+  it('night/day view click', () => {
+    cy.get('#theme-toggle-button').click();
+    
+  });
 
-  })
-
-  it('sign up', () => {
-  cy.get('a[href="/signup"] button.btn.btn-primary').click();
-})
-
-
-
-
-
-
-})
-
+  // it('sign up', () => {
+  //   cy.get('a[href="/signup"] button.btn.btn-primary').click();
+  //   cy.get('#email').type('fake@email.com').should('have.value', 'fake@email.com');
+  //   cy.get('#password').type('fake').should('have.value', 'fake');
+  //   cy.get('#submit').click();
+  // });
+  it('log in', () => {
+    cy.get('.d-flex > a:nth-child(1)').click();
+    cy.get('#email').type('fake@email.com').should('have.value', 'fake@email.com');
+    cy.get('#password').type('fake');
+    cy.get('#submit').click();
+    cy.get('.navbar-brand').click();
+  });
+  // it('create note', () => {
+  //   cy.get('.d-flex > a:nth-child(1)').click();
+  //   cy.get('#email').type('fake@email.com').should('have.value', 'fake@email.com');
+  //   cy.get('#password').type('fake');
+  //   cy.get('#submit').click();
+  //   cy.get('.navbar-brand').click();
+  // });
+});
