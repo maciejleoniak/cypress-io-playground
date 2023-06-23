ad68bea71553fe960abc27b64ea3f35518b21cf9
7799043 <leoniak.maciek1@gmail.com>
2023-06-23 08:18:07 +0200

    deleteNote

---
+++ cypress/e2e/3-note-app/deleteNote.cy.js
@@ -0,0 +1,15 @@
+context('createNote', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000');
+    });
+  
+    it('create note', () => {
+      cy.get('.d-flex > a:nth-child(1)').click();
+      cy.get('#email').type('fake@email.com').should('have.value', 'fake@email.com');
+      cy.get('#password').type('fake');
+      cy.get('#submit').click().should('be.visible');
+      cy.wait(3000);
+      
+      cy.get('.active').click();
+
+    })})
