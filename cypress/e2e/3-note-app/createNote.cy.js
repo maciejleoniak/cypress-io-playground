e3fb241e07bbf3700c3a64bfedcc30ea25a0e6c7
7799043 <leoniak.maciek1@gmail.com>
2023-06-23 08:17:00 +0200

    .

--- cypress/e2e/3-note-app/createNote.cy.js
+++ cypress/e2e/3-note-app/createNote.cy.js
@@ -1,13 +1,21 @@
 context('createNote', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:3000');
-    });
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('create note', () => {
+    cy.get('.d-flex > a:nth-child(1)').click();
+    cy.get('#email').type('fake@email.com').should('have.value', 'fake@email.com');
+    cy.get('#password').type('fake');
+    cy.get('#submit').click().should('be.visible');
+    cy.wait(3000);
     
-  // it('create note', () => {
-  //   cy.get('.d-flex > a:nth-child(1)').click();
-  //   cy.get('#email').type('fake@email.com').should('have.value', 'fake@email.com');
-  //   cy.get('#password').type('fake');
-  //   cy.get('#submit').click();
-  //   cy.get('.navbar-brand').click();
-  // });
-});
+    cy.get('.active').click();
+    cy.get('#title').type('Test Title');
+    cy.get('#desc').type('Test Description');
+    cy.get('#submit').click();
+    cy.contains('.mynotes', 'Test Title').should('be.visible');
+    cy.contains('.mynotes', 'Test Description').should('be.visible');
+  });
+
+})
